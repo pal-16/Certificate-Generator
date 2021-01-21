ca4381589012ccfb62a334b819ac0cb865a8c0b5
pal-16 <mantrypalak@gmail.com>
2021-01-21 16:15:19 +0530

    updated code for generating certificate

---
+++ README.md
@@ -0,0 +1,16 @@
+# CertificateGenerator
+A python script that will generate certificates of events and send mail to the participants just by giving an excel sheet as an input
+
+Firstly by executing the loc_config.py we find the location where the text is to be put by hit and trial
+
+Once we have the location 
+
+We use that location in auto_config.py 
+
+We give excel sheet database name and certificate image as the input and execute and also create a directory to store the generated certificate
+
+We have to put in the gmail account details through which you want to send mail and configure gmail to allow less secure apps to access it
+once all settings are proper you are good to go
+
+execute the file 
+PS i sent around 3000 mails in just a span on 10 mins
---
+++ auto_generator.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import smtplib 
+import string
+
+from email.mime.multipart import MIMEMultipart 
+from email.mime.text import MIMEText 
+from email.mime.base import MIMEBase 
+from email import encoders 
+
+from PIL import Image, ImageDraw, ImageFont
+from pandas import ExcelWriter
+from pandas import ExcelFile
+
+password ="######" #Your Password
+fromaddr = "#####" #Your Email
+df = pd.read_excel('sample-excel-sheet.xlsx')   # Your excel sheet with Participants Name and Email
+
+
+for i in df.index:
+	image = Image.open('########.jpg')           #Your certficate template
+	draw = ImageDraw.Draw(image)
+	font = ImageFont.truetype('fonts/GoogleSans-Regular.ttf', size=50)
+	color = 'rgb(0, 230, 118)'
+	name = df['Name'][i] 
+	name.upper()
+	print(i+1,name)
+	length=len(name)
+	x=1056/2-length*25/2
+	print(x)
+	draw.text((x, 300), name, fill=color, font=font)	
+	temp="sample"
+	imageName = "certificates/"+temp+".pdf"
+	image.save(imageName)
+	
+	toaddr = df['Email'][i]
+	msg = MIMEMultipart() 
+	msg['From'] = "#####"   #Your Email Id 
+	msg['Subject'] = "Certificate for Completing your Android Study Jam"
+	body = '''Please find attached your certificate for completion of Kotlin Course.
+			  
+	Regards
+	DSC 
+			'''
+
+	msg.attach(MIMEText(body, 'plain')) 
+
+	filename = name +".pdf"
+	attachment = open(imageName, "rb")
+	p = MIMEBase('application', 'octet-stream') 
+
+	p.set_payload((attachment).read()) 
+	encoders.encode_base64(p) 
+	p.add_header('Content-Disposition', "attachment; filename= %s" % filename) 
+	msg.attach(p) 
+
+	s = smtplib.SMTP('smtp.gmail.com', 587) 
+	s.starttls() 
+	s.login(fromaddr, password) 
+	text = msg.as_string() 
+	s.sendmail(fromaddr, toaddr, text) 
+
+s.quit() 
+ 
